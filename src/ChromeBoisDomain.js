3782fac203156e1d82fa387411cb6dc202661360
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2021-01-25 02:52:32 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -8,7 +8,7 @@ export default class ChromeBoisDomain extends Component {
     drawChromeBoiAtCoords(event.clientX, event.clientY)
   }
 
-  
+
     /* TODO: This method should capture the `x` and `y` coordinates of the mouse
      * from the event and use them to invoke the `drawChromeBoiAtCoords`
      * function that has been provided and is already imported
